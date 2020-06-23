f73b1d8f6fc83e36b5d3a72b7d9bd607f6752596
ddamddi <leonardkkh@gmail.com>
2020-06-23 18:25:09 +0900

    SRCNN

--- ops.py
+++ ops.py
@@ -1,19 +1,15 @@
 import tensorflow as tf
 
 w_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.001)
-b_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.0)
+b_initializer = tf.constant_initializer(0)
 
 def conv(x, channels, kernel=4, stride=2, padding='SAME', use_bias=True, scope='conv_0'):
     with tf.variable_scope(scope):
         x = tf.layers.conv2d(inputs=x, filters=channels,
                              kernel_size=kernel, kernel_initializer=w_initializer,
                              strides=stride, use_bias=use_bias, padding=padding)
-
         return x
 
 def relu(x):
     return tf.nn.relu(x)
-
-
-
 
